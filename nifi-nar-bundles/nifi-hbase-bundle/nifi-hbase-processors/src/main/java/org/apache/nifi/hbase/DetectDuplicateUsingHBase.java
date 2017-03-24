b5fc46297f270a9577ac9a2ab99b413f7cf9fe55
baolsen <bjorn.olsen1@gmail.com>
2017-03-24 14:51:16 +0200

    HBase cache basic functionality completed

--- nifi-nar-bundles/nifi-hbase-bundle/nifi-hbase-processors/src/main/java/org/apache/nifi/hbase/DetectDuplicateUsingHBase.java
+++ nifi-nar-bundles/nifi-hbase-bundle/nifi-hbase-processors/src/main/java/org/apache/nifi/hbase/DetectDuplicateUsingHBase.java
@@ -40,7 +40,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-
+import org.apache.nifi.components.AllowableValue;
 import org.apache.nifi.expression.AttributeExpression.ResultType;
 import org.apache.nifi.logging.ComponentLog;
 import org.apache.commons.lang3.StringUtils;
@@ -49,16 +49,32 @@ import org.apache.nifi.hbase.HBaseClientService;
 import org.apache.nifi.hbase.scan.ResultCell;
 import org.apache.nifi.hbase.scan.ResultHandler;
 import org.apache.nifi.hbase.scan.Column;
+import org.apache.nifi.hbase.put.PutColumn;
 
 // import java.nio.charset.Charset;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-@Tags({"example"})
-@CapabilityDescription("Provide a description")
+@Tags({"dedupe", "dupe", "duplicate", "hash", "hbase", "get"})
+@CapabilityDescription(
+   "Using HBase, caches a value, computed from FlowFile attributes, for each incoming FlowFile and determines if the cached value has already been seen. " +
+   "If so, routes the file to 'duplicate' "
+   
+)
 @SeeAlso({})
 @ReadsAttributes({@ReadsAttribute(attribute="", description="")})
 @WritesAttributes({@WritesAttribute(attribute="", description="")})
 public class DetectDuplicateUsingHBase extends AbstractProcessor {
+
+    public static final AllowableValue UPDATE_CACHE_ALWAYS = new AllowableValue("Always", "Always",
+	"Cache will always be updated, whether or not an entry exists already.");
+
+    public static final AllowableValue UPDATE_CACHE_NEW = new AllowableValue("Non-duplicate", "Non-duplicate",
+        "Cache will be updated only if an entry does NOT exist already.");
+
+    public static final AllowableValue UPDATE_CACHE_NEVER = new AllowableValue("Never", "Never",
+        "processor would only check for duplicates and not cache the Entry Identifier, requiring another " +
+        "processor to add identifiers to the HBase cache.");
 
     public static final PropertyDescriptor CACHE_ENTRY_IDENTIFIER = new PropertyDescriptor.Builder()
             .name("Cache Entry Identifier")
@@ -69,6 +85,14 @@ public class DetectDuplicateUsingHBase extends AbstractProcessor {
             .addValidator(StandardValidators.createAttributeExpressionLanguageValidator(ResultType.STRING, true))
             .defaultValue("${hash.value}")
             .expressionLanguageSupported(true)
+            .build();
+
+    public static final PropertyDescriptor UPDATE_CACHE_METHOD = new PropertyDescriptor.Builder()
+            .name("Update cache method")
+            .description("How/when the cache should be updated. ") 
+            .required(true)
+            .allowableValues(UPDATE_CACHE_ALWAYS, UPDATE_CACHE_NEW, UPDATE_CACHE_NEVER)
+            .defaultValue(UPDATE_CACHE_ALWAYS.getValue())
             .build();
 
     static final PropertyDescriptor HBASE_CLIENT_SERVICE = new PropertyDescriptor.Builder()
@@ -86,16 +110,21 @@ public class DetectDuplicateUsingHBase extends AbstractProcessor {
             .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
             .expressionLanguageSupported(true)
             .build();
-/*
-    public static final PropertyDescriptor HBASE_COLUMN_FAMILY = new PropertyDescriptor.Builder()
-            .name("HBase column family")
+
+    public static final PropertyDescriptor COLUMN_MAPPING = new PropertyDescriptor.Builder()
+            .name("HBase column mapping")
             .description(
-                    "Name of the HBase column family to use for the cache ")
+                    "Comma-separated list which maps HBase columns to attributes. Each element in the list is of the form " +
+                    "<columnFamily>:<columnQualifier>=<value>. The list may contain Attribute Expression Language statements " +
+                    ", which will be evaluated against a FlowFile. " +
+                    "Each element is stored in HBase against the Cache Entry Identifier for the FlowFile."
+                    )
             .required(true)
             .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
             .expressionLanguageSupported(true)
+            .defaultValue("f:filename=${filename},f:uuid=${uuid}")
             .build();
-*/
+
     public static final Relationship REL_DUPLICATE = new Relationship.Builder()
             .name("duplicate")
             .description("If a FlowFile has been detected to be a duplicate, it will be routed to this relationship")
@@ -118,9 +147,10 @@ public class DetectDuplicateUsingHBase extends AbstractProcessor {
     protected void init(final ProcessorInitializationContext context) {
         final List<PropertyDescriptor> descriptors = new ArrayList<PropertyDescriptor>();
         descriptors.add(CACHE_ENTRY_IDENTIFIER);
-        // descriptors.add(HBASE_TABLE_NAME);
-        // descriptors.add(HBASE_COLUMN_FAMILY);
+        descriptors.add(UPDATE_CACHE_METHOD);
         descriptors.add(HBASE_CLIENT_SERVICE);
+        descriptors.add(HBASE_TABLE_NAME);
+        descriptors.add(COLUMN_MAPPING);
         this.descriptors = Collections.unmodifiableList(descriptors);
 
         final Set<Relationship> relationships = new HashSet<Relationship>();
@@ -164,10 +194,11 @@ public class DetectDuplicateUsingHBase extends AbstractProcessor {
        
         final String tableName = context.getProperty(HBASE_TABLE_NAME).evaluateAttributeExpressions(flowFile).getValue();
         final HBaseClientService hBaseClientService = context.getProperty(HBASE_CLIENT_SERVICE).asControllerService(HBaseClientService.class); 
-       
-        final RowHandler handler = new RowHandler();
+      
+        // Check cache for ID 
+        final RowCountHandler handler = new RowCountHandler();
 
-        final String rowId="";
+        final String rowId=cacheKey;
         final byte[] rowIdBytes=rowId.getBytes(StandardCharsets.UTF_8);
 
         final List<Column> columnsList = new ArrayList<Column>(0);
@@ -175,32 +206,70 @@ public class DetectDuplicateUsingHBase extends AbstractProcessor {
 	try {
             hBaseClientService.scan(tableName, rowIdBytes, rowIdBytes, columnsList, handler);
         } catch (Exception e) {
-            getLogger().error("Unable to fetch row {} from  {} due to {}", new Object[] {rowId, tableName, e});
+            session.getProvenanceReporter().route(flowFile, REL_FAILURE);
+            logger.error("Unable to fetch row {} from  {} due to {}", new Object[] {rowId, tableName, e});
             session.transfer(flowFile, REL_FAILURE);
             return;
         }
 
-// Below here wont work
-        /*
-        final String originalCacheValue;
- 
-        final boolean shouldCacheIdentifier = context.getProperty(CACHE_IDENTIFIER).asBoolean();
-        if (shouldCacheIdentifier) {
-                originalCacheValue = cache.getAndPutIfAbsent(cacheKey, cacheValue, keySerializer, valueSerializer, valueDeserializer);
-        } else {
-                originalCacheValue = cache.get(cacheKey, keySerializer, valueDeserializer);
-        }*/
- 
+        final boolean duplicate = (handler.handledRow > 0);
+
+        // Add ID to the cache
+        final String updateCacheMethod = context.getProperty(UPDATE_CACHE_METHOD).getValue();
+        final String columnMapping = context.getProperty(COLUMN_MAPPING).evaluateAttributeExpressions(flowFile).getValue();
+
+        final boolean updateCache = ( UPDATE_CACHE_ALWAYS.equals(updateCacheMethod) || (UPDATE_CACHE_NEW.equals(updateCacheMethod) && !duplicate));
+        try {
+            if (updateCache) updateCache(hBaseClientService, tableName, rowIdBytes, columnMapping);
+        } catch (Exception e) {
+            session.getProvenanceReporter().route(flowFile, REL_FAILURE);
+            logger.error("Unable to update HBase table {} row {} due to {}", new Object[] {tableName, rowId, e});
+            session.transfer(flowFile, REL_FAILURE);
+        }
+
+        if (duplicate) {
+            // TODO originalFlowFileDescription
+            session.getProvenanceReporter().route(flowFile, REL_DUPLICATE);
+            session.transfer(flowFile, REL_DUPLICATE);
+            logger.info("Found {} to be a duplicate of FlowFile ", new Object[]{flowFile});
+            session.adjustCounter("Duplicates Detected", 1L, false);
+        }
+        else {
+            session.getProvenanceReporter().route(flowFile, REL_NON_DUPLICATE);
+            session.transfer(flowFile, REL_NON_DUPLICATE);
+            logger.info("Could not find a duplicate entry in cache for {}; routing to non-duplicate", new Object[]{flowFile});
+            session.adjustCounter("Non-Duplicate Files Processed", 1L, false);
+        }
     }
 
-    private static class RowHandler implements ResultHandler {
-        private boolean handledRow = false;
+    private void updateCache(HBaseClientService hBaseClientService, String tableName, byte[] rowIdBytes, String columnMapping) throws IOException {
+        final String[] mappings = (columnMapping == null || columnMapping.isEmpty() ? new String[0] : columnMapping.split(","));
+
+        List<PutColumn> putColumns = new ArrayList<PutColumn>(mappings.length);
+
+        for (final String mapping : mappings) {
+            final String[] kv = mapping.split("=");
+            final String[] fq = kv[0].split(":");
+           
+            final byte[] columnFamilyBytes = fq[0].getBytes(StandardCharsets.UTF_8);
+            final byte[] columnQualifierBytes = fq[1].getBytes(StandardCharsets.UTF_8);
+            final byte[] valueBytes = kv[1].getBytes(StandardCharsets.UTF_8);
+
+            final PutColumn putColumn = new PutColumn(columnFamilyBytes, columnQualifierBytes, valueBytes);
+            putColumns.add(putColumn);
+        }
+
+        hBaseClientService.put(tableName, rowIdBytes, putColumns);
+    }
+
+    private static class RowCountHandler implements ResultHandler {
+        private int handledRow = 0;
 
         @Override
         public void handle(byte[] row, ResultCell[] resultCells) {
-            handledRow = true;
+            handledRow += 1;
         }
-        public boolean handledRow() {
+        public int handledRow() {
             return handledRow;
         }
 
